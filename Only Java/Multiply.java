dba238f07e2ccc5425e5fef3c994274099b1d23e
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-23 14:10:17 +0530

    Create Multiply.java

---
+++ Only Java/Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply{
+    public static void main(String[] args){
+        int a = 90;
+        int b = 78;
+        int Mul = a*b;
+        System.out.println("Sum is "+Mul);
+    }
+}
