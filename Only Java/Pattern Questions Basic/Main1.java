43b6e0407b2051aa8afb26e3c8db1f4af0973b9a
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 14:08:48 +0530

    Create Main1.java

---
+++ Only Java/Pattern Questions Basic/Main1.java
@@ -0,0 +1,20 @@
+public class Main1{
+    public static void main(String[] args){
+        System.out.println("Pattern will be as follows ");
+        int n= 4;
+        int a =4 ;
+    for(int i =1 ; i<=n;i++){
+        for(int j= 1 ; j<=a; j++){
+            if(i==1 || j==1 || i==n || j==n){
+                System.out.print(" * ");
+            }
+            else{
+                System.out.print("   ");
+            }
+            
+        
+    }System.out.println("");
+
+    }
+    }
+}
