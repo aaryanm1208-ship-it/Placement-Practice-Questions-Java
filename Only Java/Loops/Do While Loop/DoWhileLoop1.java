3c7a388a7b53cfdea99716ea34f36555447e281a
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 11:58:55 +0530

    Create DoWhileLoop1.java

---
+++ Only Java/Loops/Do While Loop/DoWhileLoop1.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop1{
+    public static void main(String[] args){
+        System.out.println("Be ready For loops");
+int i = 0;
+do{
+    System.out.println(i);
+    i++;
+}
+while(i<=11);
+}
+}
