7150de0c154288d39f84e104bf917ae6d6c525f5
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 11:56:04 +0530

    Create WhileLoop1.java

---
+++ Only Java/Loops/While Loop/WhileLoop1.java
@@ -0,0 +1,10 @@
+public class WhileLoop1{
+    public static void main(String[] args){
+        System.out.println("Be ready For loops");
+int i = 0;
+while(i<=11){
+    System.out.println(i);
+    i++;
+}
+}
+}
