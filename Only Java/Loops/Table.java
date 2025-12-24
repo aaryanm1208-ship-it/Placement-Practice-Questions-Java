f3c2966a551db4d06bf03a688991a661cacf8629
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 12:18:54 +0530

    Create Table.java

---
+++ Only Java/Loops/Table.java
@@ -0,0 +1,9 @@
+public class Table{
+    public static void main(String[] args){
+        System.out.println("Table for the number is ");
+        int j=3;
+        for(int i = 1 ; i <=10 ; i++){
+            System.out.println(i*j);
+        }
+    }
+}
