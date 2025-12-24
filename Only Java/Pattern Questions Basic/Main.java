d676be8fbe0e5f5d1ae47e65922c6b5390b68495
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-24 13:51:38 +0530

    Create Main.java

---
+++ Only Java/Pattern Questions Basic/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Pattern will be as follows ");
+        int n= 5;
+        int a = 8;
+    for(int i =0 ; i<n;i++){
+        for(int j= 0 ; j<a; j++){
+        System.out.print("*");
+    }System.out.println();
+
+    }
+    }
+}
